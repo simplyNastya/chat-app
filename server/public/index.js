f0df5e6bdace6c5b8341e2de9fe96a32cc3e40dd
Anastasiia Koretska <koretska.anastasiia@gmail.com>
2023-10-31 22:11:39 +0100

    deploy

--- server/public/index.js
+++ server/public/index.js
@@ -1,4 +1,4 @@
-const socket = io('ws://localhost:3500')
+const socket = io('https://chat-app-3k4m.onrender.com')
 
 const msgInput = document.querySelector('#message')
 const nameInput = document.querySelector('#name')
